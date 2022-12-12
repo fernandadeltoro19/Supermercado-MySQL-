ed66ddc4b820b9553ac1a8c5dc9e04b98b686a35
fernandadeltoro19 <I20050585@monclova.tecnm.mx>
2022-12-11 22:16:20 -0600

    Agregar archivos de proyecto.

---
+++ PruebaMySQL/ConexionMySql.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+
+namespace PruebaMySQL
+{
+    class ConexionMySql
+    {
+
+        public static MySqlConnection conexion = new MySqlConnection();
+
+        static string servidor = "127.0.0.1";
+        static string bd = "supermercado";
+        static string ususario = "root";
+        static string password = "";
+        static string puerto = "3306";
+
+         static String cadenaConexion = "server=" + servidor + ";" + "port=" + puerto + ";" + "user id =" + ususario + ";" + "password=" + password + ";" + "database=" + bd + ";";
+
+        private static void conectar()
+        {
+            try
+            {
+                conexion.ConnectionString = cadenaConexion;
+                conexion.Open();
+                //MessageBox.Show("Se conecto correctamente la base de datos");
+            }
+            catch (MySqlException e)
+            {
+                MessageBox.Show("No se puede conectar a la base de datos de PostgreSQL, error:" + e.ToString());
+            }
+        }
+
+        public static DataTable ejecutaConsultaSelect(string consulta)
+        {
+            conectar();
+            MySqlDataAdapter adaptador = new MySqlDataAdapter(consulta, conexion);
+            DataSet ds = new DataSet();
+            adaptador.Fill(ds, "tabla");
+            conexion.Close();
+            return ds.Tables["tabla"];
+        }
+
+        public static void ejecutaConsulta(string consulta)
+        {
+            conectar();
+            MySqlCommand comando = new MySqlCommand(consulta, conexion);
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+
+
+    }
+}
